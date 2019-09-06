84df2d1c4b10815f3dd57592c2b85727d26864b4
Eric Tan <EricTan1>
2019-09-06 00:35:37 -0400

    Fixed alot of formatting issues and also implemented connect,disconnect and close

--- App.py
+++ App.py
@@ -5,16 +5,17 @@ import json
 import sys
 import os
 import aiohttp
+import youtube_dl
 from discord.ext import commands
 # adding the paths of the different modules in the different folders
 # sys.path.append('./GambleGames/')
 
 # Setting global variables
 _command_prefix = '$'
+players = {}
 
 # create new discord bot obj
 client = commands.Bot(command_prefix=_command_prefix)
-
 
 # read the information for the api keys and put them in a dictionary
 _key_dict = None
@@ -45,21 +46,59 @@ async def set_messageInt(messageInitializer):
     global _messageInitializer
     _messageInitializer = messageInitializer
 
-@client.command(pass_context=True)
+@client.command()
 async def goodnight(ctx):
     await client.send_message(ctx.message.channel, "GoodNight!")
 
-@client.command(pass_context=True)
+@client.command()
 async def connect(ctx):
-    channel = ctx.message.author.voice.voice_channel
-    await client.join_voice_channel(channel)
+    # get the voice channel of the author and connect to it
+    # if there is no voice channel then send an error message
+    author_voice = ctx.message.author.voice
+    if(author_voice == None):
+        temp_embed = discord.Embed()
+        temp_embed.color = 15158332
+        temp_embed.title = "Error"
+        temp_embed.description = "You are not currently in a voice channel"
+        await ctx.send(embed=temp_embed)
+    # if the bot is already in a voice channel
+    elif(ctx.guild.voice_client != None):
+        print("already in a voice channel")
+        # if the author of the message isn't already in the same voice channel as the bot
+        if(ctx.guild.voice_client.channel != author_voice.channel):
+            await ctx.guild.voice_client.disconnect()
+            await author_voice.channel.connect()
+    # connect the bot to a voice channel of the author
+    else:
+        await author_voice.channel.connect()
+        
 
 
-@client.command(pass_context=True)
+@client.command()
 async def disconnect(ctx):
+    # disconnect the bot from the voice if it is connected
+    if(ctx.guild.voice_client != None):
+        await ctx.guild.voice_client.disconnect()
+
+
+@client.command()
+async def close(ctx):
+    # embeded message to show that the bot is shut down
+    temp_embed = discord.Embed()
+    temp_embed.color = 3066993
+    temp_embed.title = "Closed"
+    temp_embed.description = "Bot has been successfully closed"
+    await ctx.send(embed=temp_embed)
+    # shut down the bot
+    await client.close()
+
+@client.command()
+async def play(ctx, url):
     server = ctx.message.server
     voice_client = client.voice_client_in(server)
-    await voice_client.disconnect()
+    player = await voice_client.create_ytdl_player(url)
+    players[server.id] = player
+    player.start()
 
 # Run the bot
 client.run(TOKEN)
