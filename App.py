4088a67191f1b9455d39e754f86826cf75ee32b8
Eric Tan <EricTan1>
2019-09-07 21:41:49 -0400

    finished MVP for amq

--- App.py
+++ App.py
@@ -170,10 +170,11 @@ async def game(ctx, game):
         await channel.send('Hello {.author}!'.format(msg))
         participants = ctx.guild.voice_client.channel.members
         print(participants)
-        current_game = Amq(client, participants, ctx.message.channel, ctx.guild.voice_client, MusicPlayer(client), rounds=10, time_sec=20.0)
-        print("created amq obj")
+        current_game = Amq(client, participants, ctx.message.channel, ctx.guild.voice_client, MusicPlayer(client), rounds=5, time_sec=35.0)
+        client.add_cog(current_game)
         await current_game.set_up()
         await current_game.play_game()
+        # remove cog after game finishes
 
 
 
--- Games/Amq.py
+++ Games/Amq.py
@@ -14,28 +14,33 @@ from importlib import reload
 import aiohttp
 
 
-class Amq(Game):
+class Amq(commands.Cog):
     ''' This class is responsible for the entire anime music quiz game
     
     '''
 
-    def __init__(self, bot, participants, text_channel, voice_channel, music_player, rounds=5, time_sec=20.0):
+    def __init__(self, bot, participants, text_channel, voice_channel, music_player, rounds=3, time_sec=20.0):
         ''' (Amq, discord.Client, List of Strings, int, double,) -> Amq
         '''
-        super().__init__(bot)
+        # super().__init__(bot)
+        self.bot = bot
         self.rounds = rounds
         self.music_player = music_player
         self.text_channel = text_channel
         self.voice_channel = voice_channel
         self.time_per_song = time_sec
+        self.test = None        #p_list = []
+        # for person in participants:
+            #p_list.append(Player(person.id, person.name, person.mention))
         self.participants = participants
-        
+
+
     async def set_up(self):
         p_list = []
         anime_list = []
         # initializing the player list
         for person in self.participants:
-            p_list.append(Player(person.id))
+            p_list.append(Player(person.id, person.name, person.mention))
             # Union two sets (no repeats)
             #import AniList
             second_list = []
@@ -53,20 +58,26 @@ class Amq(Game):
             in_second = set(second_list)
             in_second_but_not_in_first = in_second - in_first
             anime_list = anime_list + list(in_second_but_not_in_first)
-            print(anime_list)
-            
-        self.participiants = p_list
+
+        self.participants = p_list
         self.aniList_personal = anime_list
         # RANDO ANIME CHOOSING CODE HERE
         self.animeQueue = Queue(self.rounds)
         for x in range(self.rounds):
-
                 song_name = self.aniList_personal.pop(randrange(len(self.aniList_personal) - 1))
                 if(song_name != None):
-                    self.animeQueue.put(Song(song_name, "temp", "temp"))
-                    print("Queue: " + song_name)        
+                    res = await get_aniListAnime(song_name)
+                    anime_data = res.get("data").get("Media")
+                    name_types = set()
+                    name_types.add(anime_data.get('title').get('english'))
+                    name_types.add(anime_data.get('title').get('romaji'))
+                    name_types.add(anime_data.get('title').get('native'))
+                    pic_url = anime_data.get('coverImage').get('medium')
+                    site_url = anime_data.get('siteUrl')
+                    desc = anime_data.get('description')
+                    self.animeQueue.put(Song(anime_data.get('title').get('english'), name_types, pic_url, site_url, desc))
+
     async def play_game(self):
-        print("playing game")
         while (not self.animeQueue.empty()):
             current_song = self.animeQueue.get()
             # play the song
@@ -75,14 +86,11 @@ class Amq(Game):
                 # if the bot currently has audio on then stop it
                 if(self.text_channel.guild.voice_client.is_playing()):
                     self.text_channel.guild.voice_client.stop()
-                await self.text_channel.send("STARTING DOWNLOAD")
-                audio_source = discord.FFmpegPCMAudio(self.music_player.download("test_song", current_song.name + " OPENING"))
-                await self.text_channel.send("DONE GETTING STREAM URL")
+                audio_source = discord.FFmpegPCMAudio(self.music_player.download("test_song", current_song.english_name + " opening tv size"))
                 await self.music_player.play_music(audio_source, self.text_channel, self.voice_channel)
-                await self.text_channel.send("PLAYING SONG")
+                await self.text_channel.send("Playing Song")
 
             channel = self.text_channel
-            await self.text_channel.send("Sleeping")
 
             await channel.send('Send me that \U0001f44e reaction, to skip')
 
@@ -92,39 +100,80 @@ class Amq(Game):
             try:
                 reaction, user = await self.bot.wait_for('reaction_add', timeout=self.time_per_song, check=check)
             except asyncio.TimeoutError:
-                await self.text_channel.send("done sleeping")
-                await self._calculate_round_results(current_song)
+                await self.text_channel.send("Song is done")
             else:
-                await self.text_channel.send("done sleeping")
+                await self.text_channel.send("Song is done")
                 await channel.send('Current Song has been skipped \U0001f44e')
             # if the bot currently has audio on then stop it
             if(self.text_channel.guild.voice_client.is_playing()):
                 await self.music_player.stop_music(self.text_channel)
             await self.text_channel.send("stopped playing")
+            await self._calculate_round_results(current_song)
+        await self.end_game()
             
-                
+
     async def _calculate_round_results(self, current_song):
         # check answers and reward points
-        for players in self.participiants:
-            if(players.answer != None):
-                if(players.answer.upper() == current_song.name.upper()):
-                    player.score = player.score + 1
+        for players in self.participants:
+            if(players.guess != None):
+                if(players.guess in current_song.name):
+                    await self.text_channel.send(players.mention + " guessed it correctly!")
+                    players.score = players.score + 1
+                # reset the guesses
+                players.guess = None
         # send answer
-        await self.text_channel.send("The anime was {}".format(current_song.name))
-    
+        temp_embed = discord.Embed()
+        anime_title = ""
+        for titles in current_song.name:
+            anime_title = anime_title + titles + " | "
+
+        temp_embed.title = anime_title
+        temp_embed.color = 3900661        
+        temp_embed.url = current_song.website_url
+        temp_embed.set_image(url=current_song.picture_url)
+        temp_embed.description = current_song.description
+        await self.text_channel.send("The correct answer is: ", embed=temp_embed)
+
+    async def end_game(self):
+        # send answer
+        temp_embed = discord.Embed()
+        temp_embed.title = "Anime Music Quiz Results"
+        temp_embed.color = 4652906
+        for players in self.participants:
+            temp_embed.add_field(name=players.name, value="Score: " + str(players.score), inline="false")
+        await self.text_channel.send(embed=temp_embed)
+    @commands.command()
+    async def guess(self, ctx, *guess):
+        search = ' '.join(guess)
+        for players in self.participants:
+            if(players.id == ctx.message.author.id):
+                print(players.score)
+                players.guess = search
+
+    @commands.command()
+    async def search(self, ctx, anime):
+        res = await get_aniListAnime(anime)
+        anime_data = res.get("data").get("Media")
+        anime_data.get('title').get('english')
+        anime_data.get('title').get('romaji')
+        await self.text_channel.send("Did you mean: {} or {}".format(anime_data.get('title').get('english'), anime_data.get('title').get('romaji')))
 
 class Player():
-    def __init__(self, id):
+    def __init__(self, id, name, mention, guess=None):
         self.score = 0
         self.id = id
-        self.answer = None
+        self.name = name
+        self.mention = mention
+        self.guess = guess
 
 
 class Song():
-    def __init__(self, name: str, picture_url: str, website_url: str):
+    def __init__(self, english_name, name: set, picture_url: str, website_url: str, description: str):
         self.name = name
+        self.english_name = english_name
         self.picture_url = picture_url
         self.website_url = website_url
+        self.description = description
 
 async def get_aniList(user):
     query = query = '''
@@ -134,7 +183,6 @@ async def get_aniList(user):
             name
             entries{
                 media{
-                    bannerImage
                     siteUrl
                     coverImage{
                         medium
@@ -154,6 +202,36 @@ async def get_aniList(user):
     variables = {
         'userName': user,
         'forceSingleCompletedList': True
+    }
+
+    url = 'https://graphql.anilist.co/'
+    ret = []
+    async with aiohttp.ClientSession() as cs:
+        async with cs.request('POST', url, json={'query': query, 'variables': variables}) as r:
+            response = await r.json()  # returns dict
+
+    return response
+
+async def get_aniListAnime(anime_name):
+    query = query = '''
+    query ($search: String) { 
+      Media (search:$search,type: ANIME) {
+            siteUrl
+            description
+            coverImage{
+                medium
+            }
+            title{
+                english
+                romaji
+                native
+            }
+                
+        }
+      }
+    '''
+    variables = {
+        'search': anime_name,
     }
 
     url = 'https://graphql.anilist.co/'
